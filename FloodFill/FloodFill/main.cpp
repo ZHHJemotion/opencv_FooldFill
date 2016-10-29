a0101d802da4f99005cd6aab211a6c9d87c9cd8d
zhhjemotion <zhhjemotoin@hotmail.com>
2016-10-29 21:17:44 +0200

    FloodFill 漫水填充

--- FloodFill/FloodFill/main.cpp
+++ FloodFill/FloodFill/main.cpp
@@ -7,9 +7,28 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#define PATH string("/Users/zhangxingjian/Desktop/Programming/C++/OpenCV/opencv_FooldFill/FloodFill/FloodFill/")
+
+using namespace cv;
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    Mat src = imread(PATH+string("1.jpg"),1);
+    namedWindow("原始图");
+    imshow("原始图", src);
+    Rect ccomp;
+    floodFill(src, Point(50,300), Scalar(155,255,55), &ccomp, Scalar(20,20,20), Scalar(20,20,20));
+    imshow("原始图",  src);
+    waitKey(0);
+    
+    
     return 0;
 }
